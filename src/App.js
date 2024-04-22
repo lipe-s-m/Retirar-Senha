2a727169444545fddce399d64f3ac67acd50e0cd
lipe-s-m <felipe.s.m1609@gmail.com>
2024-04-22 16:27:51 -0300

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
-import React, {useState} from 'react';
 
 function App() 
 {
-  const [name, setName] = useState()
-  const [email, setEmail] = useState()
-  const [profilePic, setProfilePic] = useState()
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return <GoogleLogin
   onSuccess={credentialResponse => {
